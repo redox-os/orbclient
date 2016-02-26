ff633b890af9d00fd49928e6bd6435633344434d
Jeremy Soller <jackpot51@gmail.com>
2016-02-26 09:16:48 -0700

    Add data function for arbitrary pixel operations

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![crate_name="orbclient"]
 #![crate_type="lib"]
 #![feature(associated_consts)]
+#![feature(const_fn)]
 #![feature(box_syntax)]
 #![feature(deprecated)]
 
@@ -13,15 +14,11 @@ pub static FONT: &'static [u8] = include_bytes!("../res/unifont.font");
 pub use bmp::BmpFile;
 pub use color::Color;
 pub use event::*;
-pub use point::Point;
-pub use size::Size;
 pub use window::Window;
 
 pub mod bmp;
 pub mod color;
 pub mod event;
-pub mod point;
-pub mod size;
 
 #[cfg(target_os = "redox")]
 #[path="orbital/window.rs"]
