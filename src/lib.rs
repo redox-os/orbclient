2cbc603720ec855083b5cc86307b81258788eb3f
Jeremy Soller <jackpot51@gmail.com>
2017-09-23 20:13:51 -0600

    Only add feature on non-redox targets

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 #![feature(alloc)]
 #![feature(asm)]
 #![feature(const_fn)]
-#![feature(const_ptr_null_mut)]
+#![cfg_attr(all(not(feature="no_std"), not(target_os = "redox")), feature(const_ptr_null_mut))]
 #![cfg_attr(feature="no_std", no_std)]
 
 #![deny(warnings)]
