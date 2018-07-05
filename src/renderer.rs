54d013694d7dfcdb989b4285195d2b8a3c96fa91
Michael Hölzl <michaelhoelzl@Michaels-MacBook-Pro-2.local>
2018-07-05 08:50:20 +0200

    [bugfix] Problems with non-square rectangles

--- src/renderer.rs
+++ src/renderer.rs
@@ -71,7 +71,7 @@ pub trait Renderer {
             
             if alpha >= 255 || replace {
                 *old = new;
-            } else if alpha >0 {
+            } else if alpha > 0 {
                 let n_alpha = 255 - alpha;
                 let rb = ((n_alpha * ( *old & 0x00FF00FF)) + (alpha * (new & 0x00FF00FF))) >> 8;
                 let ag = (n_alpha * ((*old & 0xFF00FF00) >> 8)) + ( alpha * (0x01000000 | ((new & 0x0000FF00) >>8)));
@@ -339,39 +339,43 @@ pub trait Renderer {
     }
 
     fn box_shadow(&mut self, x: i32, y: i32, w: u32, h: u32, offset_x: i32, offset_y: i32, r: i32, color: Color) {
-        let real_w = w + (2*r as u32);
-        let real_h = h + (2*r as u32);
+        let renderer_w = self.width();
+        let renderer_h = self.height();
+
+        let start_y = cmp::max(0, cmp::min(renderer_h as i32 - 1, y - r));
+        let end_y = cmp::max(start_y, cmp::min(renderer_h as i32, y + h as i32 + r));
+        let start_x = cmp::max(0, cmp::min(renderer_w as i32 - 1, x - r));
+        let end_x = cmp::max(start_x, cmp::min(renderer_w as i32, x + w as i32 + r));
 
         let mut blur_data: Vec<Color> = Vec::new();
-        for new_x in x..x+real_w as i32 {
-            for new_y in y..y+real_h as i32 {
-                if new_x < x+r || new_y < y+r || new_y >= y+h as i32 +r || new_x >= x+w as i32+r {
-                    blur_data.push(Color::rgb(255,0,255));
+        for yb in start_y..end_y {
+            for xb in start_x..end_x {
+                if xb < x || yb < y || yb >= y + h as i32 || xb >= x + w as i32 {
+                    blur_data.push(Color::rgb(255, 0, 255));
                 } else {
-                    blur_data.push(Color::rgb(0,0,0));
+                    blur_data.push(Color::rgb(0, 0, 0));
                 }
             }
         }
+
+        let real_w = end_x - start_x;
+        let real_h = end_y - start_y;
 
         blur::gauss_blur(&mut blur_data, real_w as u32, real_h as u32, r as f32 / 3.0);
 
         let mut counter: u32 = 0;
-        for new_x in (x-r)..(x+real_w as i32-r) as i32 {
-            for new_y in (y-r)..(y+real_h as i32 -r) as i32 {
-                let c = blur_data[counter as usize];
+        for yb in start_y + offset_y..end_y + offset_y {
+            for xb in start_x + offset_x..end_x + offset_x {
 
-                let mut alpha: u8 = if color.a() < 255 - c.r() { color.a() } else { 255 - c.r() };
-                let col = Color::rgba(color.r(), color.g(), color.b(), alpha);
-
-                let new_x_b = new_x + offset_x;
-                let new_y_b = new_y + offset_y;
-                if new_x_b < x || new_x_b > x+w as i32 -1 || new_y_b < y || new_y_b > y+h as i32 -1 {
-                    self.pixel(new_x_b, new_y_b, col);
+                if xb < x || yb < y || yb >= y + h as i32 || xb >= x + w as i32 {
+                    let c = blur_data[counter as usize];
+                    let mut alpha: u8 = if color.a() < 255 - c.r() { color.a() } else { 255 - c.r() };
+                    let col = Color::rgba(color.r(), color.g(), color.b(), alpha);
+                    self.pixel(xb , yb , col);
                 }
                 counter = counter + 1;
             }
         }
-
     }
 
     /// Display an image
